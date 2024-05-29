d8b19102e48be479f19d6cc3972235de804f5ea8
tatuki kosikawa <159102831+tatu-coding@users.noreply.github.com>
2024-05-29 18:40:27 +0900

    2024/5/29

--- AOJ/ITP1_6_B.cpp
+++ AOJ/ITP1_6_B.cpp
@@ -2,14 +2,47 @@
 using namespace std;
 
 int main() {
-    int tramp[4][13], value;
+    int tramp[4][13] = {0};
 
     int n;
-    std::cin >> n;
+
+    int i, k;
 
     char j;
-    int j, k;
 
+    int l, m;
 
+    cin >> n;
 
+    for(i = 0; i < n; i++) {
+        cin >> j >> k;
+
+        if (j == 'S') {
+            tramp[0][k-1] = 1;
+        } else if (j == 'H') {
+            tramp[1][k-1] = 1;
+        } else if (j == 'C') {
+            tramp[2][k-1] = 1;
+        } else if (j == 'D') {
+            tramp[3][k-1] = 1;
+        }
+    }
+
+    for(l = 0; l < 4; l++) {   
+        for(m = 0; m <= 12; m++){
+            if(tramp[l][m] != 1) {
+                if(l == 0) {
+                    cout << "S " << m+1 << endl;
+                } else if (l == 1) {
+                    cout << "H " << m+1 << endl;
+                } else if (l == 2) {
+                    cout << "C " << m+1 << endl;
+                } else if (l == 3) {
+                    cout << "D " << m+1 << endl;
+                }
+            }
+        }
+    }
+    
+    return 0;
 }
