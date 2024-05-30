41c528e011e59976b75443fd5b514605f3f627de
tatu-coding <tatuki.adel@gmail.com>
2024-05-31 08:54:37 +0900

    2024/05/31

---
+++ AOJ/ITP1_7_A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m, f, r;
+    
+    for(int i = 0; i < 50; i++) {
+        cin >> m >> f >> r;
+        if(m == -1 || f == -1) {
+            cout >> "A" >> endl;
+        } else if () {
+
+        }
+    }
+}
