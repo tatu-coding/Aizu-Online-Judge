70622ca8c5189fc647c45f540d8549359f9f096d
Koshikawa tatuki <tatuki.adel@gmail.com>
2024-05-31 13:55:20 +0900

    20240531

--- AOJ/ITP1_7_A.cpp
+++ AOJ/ITP1_7_A.cpp
@@ -6,10 +6,26 @@ int main() {
     
     for(int i = 0; i < 50; i++) {
         cin >> m >> f >> r;
-        if(m == -1 || f == -1) {
-            cout >> "A" >> endl;
-        } else if () {
-
+        if (m == -1 && f == -1 && r == -1) {
+            break;
         }
-    }
-} dafsadfsa
+
+        if(m == -1 || f == -1) {
+            cout << "F" << endl;
+        } else if (m + f >= 80) {
+            cout << "A" << endl;
+        } else if (m + f >= 65) {
+            cout << "B" << endl;
+        } else if (m + f >= 50) {
+            cout << "C" << endl;
+        } else if (m + f >= 30) {
+            if (r >= 50) {
+                cout << "C" << endl;
+            } else {
+                cout << "D" << endl;
+            }
+        } else if (m + f >= 0) {
+            cout << "F" << endl;
+        } 
+    }   
+}
