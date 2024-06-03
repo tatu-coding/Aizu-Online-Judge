78d1611cac2b3f472d4b9a46ae5a013cc0265bfe
tatuki kosikawa <159102831+tatu-coding@users.noreply.github.com>
2024-06-03 20:00:14 +0900

    Update ITP1_7_C.cpp

---
+++ AOJ/ITP1_7_D.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m, l;
+    cin >> n >> m >> l;
+
+    int detA[n][m];
+    int detB[m][l];
+    int detC[n][l];
+
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
+            cin >> detA[i][j];
+        }
+    }
+
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < l; j++){
+            cin >> detB[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < l; j++){
+            for (int k = 0; k < m; k++){
+                if (k > 0) {
+                cout << " ";
+                }
+                detC[i][j] += detA[i][k]*detB[k][l];
+            }
+            cout << detC[i][j] << endl;
+        }
+    }
+}
