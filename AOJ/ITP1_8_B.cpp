e13ebf28a24f013cab893676e241eebb0aaa0c31
tatuki kosikawa <159102831+tatu-coding@users.noreply.github.com>
2024-06-08 15:19:42 +0900

    2024/6/8

---
+++ AOJ/ITP1_8_B.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+
+    while (true) {
+        cin >> str;
+        if (str == "0") {
+            break;
+        }
+
+        int sum = 0;
+        for (int i = 0; i < str.size(); i++) {
+            sum += str[i] - '0';
+        }
+
+        cout << sum << endl;
+    }
+}
